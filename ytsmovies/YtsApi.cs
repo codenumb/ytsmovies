7500d4f436c1c082f18b0d80798e1d1004818e82
codenumb <thejaskoot@gmail.com>
2020-06-28 22:30:28 +0530

    yts api integration start

--- ytsmovies/YtsApi.cs
+++ ytsmovies/YtsApi.cs
@@ -13,33 +13,22 @@ namespace ytsmovies
         {
 
         }
-        public async Task<List<TodoItem>> RefreshDataAsync()
-        {
-            Uri uri = new Uri(string.Format(Constants.TodoItemsUrl, string.Empty));
-            HttpResponseMessage response = await client.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
-            }
-}
     }
-    public class YtsJson
+    public class Torrents
     {
-        public string status { get; set; }
-        public string status_message { get; set; }
-        public _YtsData[] data { get; set; }
-        public _YtsMeta meta { get; set; }
-    }
-    public class _YtsData
-    {
-        public int movie_count { get; set; }
-        public int limit { get; set; }
-        public _YtsMovie[] movies { get; set; }
-        public int page_number { get; set; }
+        public string url { get; set; }
+        public string hash { get; set; }
+        public string quality { get; set; }
+        public string type { get; set; }
+        public int seeds { get; set; }
+        public int peers { get; set; }
+        public string size { get; set; }
+        public long size_bytes { get; set; }
+        public string date_uploaded { get; set; }
+        public int date_uploaded_unix { get; set; }
 
     }
-    public class _YtsMovie
+    public class Movies
     {
         public int id { get; set; }
         public string url { get; set; }
@@ -49,9 +38,9 @@ namespace ytsmovies
         public string title_long { get; set; }
         public string slug { get; set; }
         public int year { get; set; }
-        public int rating { get; set; }
+        public string rating { get; set; }
         public int runtime { get; set; }
-        public string[] genres { get; set; }
+        public IList<string> genres { get; set; }
         public string summary { get; set; }
         public string description_full { get; set; }
         public string synopsis { get; set; }
@@ -64,28 +53,33 @@ namespace ytsmovies
         public string medium_cover_image { get; set; }
         public string large_cover_image { get; set; }
         public string state { get; set; }
-        public _YtsTorrent torrents { get; set; }
+        public IList<Torrents> torrents { get; set; }
         public string date_uploaded { get; set; }
-        public long date_uploaded_unix { get; set; }
+        public int date_uploaded_unix { get; set; }
+
     }
-    public class _YtsTorrent
+    public class Data
     {
-        public string url { get; set; }
-        public string hash { get; set; }
-        public string quality { get; set; }
-        public string type { get; set; }
-        public int seeds { get; set; }
-        public int peers { get; set; }
-        public string size { get; set; }
-        public long size_bytes { get; set; }
-        public string date_uploaded { get; set; }
-        public long date_uploaded_unix { get; set; }
+        public int movie_count { get; set; }
+        public int limit { get; set; }
+        public int page_number { get; set; }
+        public IList<Movies> movies { get; set; }
+
     }
-    public class _YtsMeta
+    public class Meta
     {
-        public long server_time { get; set; }
+        public int server_time { get; set; }
         public string server_timezone { get; set; }
         public int api_version { get; set; }
         public string execution_time { get; set; }
+
+    }
+    public class Ytorrent
+    {
+        public string status { get; set; }
+        public string status_message { get; set; }
+        public Data data { get; set; }
+        public Meta @meta { get; set; }
+
     }
 }
